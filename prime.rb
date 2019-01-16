129648ea6bf2d5480330d62e8a67eacdee94b04a
Brian Haltiwanger <bhalt@bellsouth.net>
2019-01-16 15:31:02 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(number)
+  if number > 1
+    if number == 2
+      return true
+    elsif number.even?
+        return false
+    else
+       range = (2...number).to_a
+       range.none? do |n|
+         number % n == 0  #remander is it equal to 0
+       end
+    end
+  end
+    return false        
+end    
